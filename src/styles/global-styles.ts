467cd639449cc0215af3dff85955efd33459972b
Adriel Borges <adrielluca@gmail.com>
2022-03-16 01:42:44 -0300

    CHORE: remove aux colors

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -4,8 +4,6 @@ export const GlobalStyle = createGlobalStyle`
     :root {
       --red: #E52E4D;
       --green: #33CC95;
-      --blue: #5429CC;
-      --blue-light: #6933FF;
       --text-title: #363F5F;
       --text-body: #969CB3;
       --background: #0A0C17;
